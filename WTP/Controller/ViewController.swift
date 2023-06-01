dc1a015b9b5810d318b3b6b4b3fcf66b3be5e43d
Jameka Echols <jameka.echols@gmail.com>
2023-05-31 22:15:18 -0400

    added stackview so that it sizes both username andpassword textfields perfectly. also added rounded borders and changed background to white

--- WTP/Controller/ViewController.swift
+++ WTP/Controller/ViewController.swift
@@ -9,61 +9,77 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private lazy var textFieldsStackViewContainer: UIStackView = {
+       let sv = UIStackView(arrangedSubviews: [userNameTextField,passwordTextField])
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.axis = .vertical
+        sv.spacing = 20
+        sv.distribution = .fillEqually
+        return sv
+    }()
+    
     var userNameTextField: UITextField = {
-       let tf = UITextField()
+        let tf = UITextField()
         tf.translatesAutoresizingMaskIntoConstraints = false
-       let textAttribute =  NSAttributedString(string: "My Placeholder",
-                                                            attributes: [NSAttributedString.Key.foregroundColor: UIColor.black])
-        tf.attributedPlaceholder = textAttribute
+        tf.placeholder = "Username"
+        tf.layer.cornerRadius = 20.5
+        tf.borderStyle = .line
+        tf.layer.borderColor = UIColor.lightGray.cgColor
+        tf.layer.borderWidth = 1.0
+        tf.layer.masksToBounds = true
+        return tf
+    }()
+    
+    var passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.isSecureTextEntry = true
+        tf.placeholder = "Password"
+        tf.layer.cornerRadius = 20.5
+        tf.borderStyle = .line
+        tf.layer.borderColor = UIColor.lightGray.cgColor
+        tf.layer.borderWidth = 1.0
+        tf.layer.masksToBounds = true
         return tf
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .cyan
+        view.backgroundColor = .white
         configureUI()
     }
     
     func configureUI(){
-       configureTextField()
+       configureTextFields()
     }
     
-    func configureTextField(){
+    func configureTextFields(){
+        view.addSubview(textFieldsStackViewContainer)
+        textFieldsStackViewContainer.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        textFieldsStackViewContainer.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        textFieldsStackViewContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        textFieldsStackViewContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
         
-        view.addSubview(userNameTextField)
-        userNameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        userNameTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        userNameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 30).isActive = true
-        userNameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
-        userNameTextField.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        userNameTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        passwordTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
         
         userNameTextField.delegate = self
+        passwordTextField.delegate = self
         
-        userNameTextField.backgroundColor = .white
-        userNameTextField.textColor = .black
-        userNameTextField.placeholder = "Username"
-        userNameTextField.borderStyle = .line
-        userNameTextField.layer.cornerRadius = 28
-        
-        userNameTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 60))
-        userNameTextField.leftViewMode = .always
     }
 
 }
 extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         //Check if there is any other text-field in the view whose tag is +1 greater than the current text-field on which the return key was pressed. If yes → then move the cursor to that next text-field. If No → Dismiss the keyboard
-        //    if let nextField = self.view.viewWithTag(textField.tag + 1) as? UITextField {
-        //        nextField.becomeFirstResponder()
-        //    } else {
-        //        textField.resignFirstResponder()
-        //    }
-        //        return false
-        //    }
-        view.endEditing(true)
-        return true
-        
+        if let nextField = self.view.viewWithTag(textField.tag + 1) as? UITextField {
+            nextField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        return false
     }
 }
+    
+
 
